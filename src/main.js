8852f0904d740bdb956582e4b7f513cd418eeb83
PotatoesMom <wangtianjingjing@gmail.com>
2019-03-27 09:45:38 +0800

    清除默认样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/css/reset.css' /*清除默认样式*/
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
